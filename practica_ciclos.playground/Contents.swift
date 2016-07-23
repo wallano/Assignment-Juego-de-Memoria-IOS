bb9fefbe8bd152d9d16e9a8f19303e2c4ef9f64f
wallano <wallano@gmail.com>
2016-07-22 19:24:35 -0500

    Add files via upload

---
+++ practica_ciclos.playground/Contents.swift
@@ -0,0 +1,22 @@
+//: Práctica semana 2
+
+import UIKit
+
+var numeros = 1...100
+
+for numero in numeros {
+
+    if numero % 5 == 0 {
+        print("#  \(numero) Bingo!!!")
+    }
+    if numero % 2 == 0 {
+        print("#  \(numero) par!!!")
+    }
+    if numero % 2 != 0 {
+        print("#  \(numero) impar!!!")
+    }
+    if numero >= 30 && numero <= 40 {
+        print("#  \(numero) Viva Swift!!!")
+    }
+
+}
